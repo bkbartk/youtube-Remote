c23bb7c6a1a7dac986d61a8ce9a346b73371132a
Bart Kock <bart.kock@sienn.com>
2021-11-29 22:00:48 +0100

    remove id

--- youtube-remote-control.js
+++ youtube-remote-control.js
@@ -48,7 +48,7 @@ class YouTubeRemoteControl extends LitElement {
 <!-- ################################# DIRECTION PAD END ################################# -->
 
                   <div class="grid-container-volume-channel-control" >
-                      <paper-input id="keypadinput" class="btn-flat flat-high ripple" style="margin-top: 0px; height: 50%;border: 2px solid white;" placeholder="Search..." @keydown="${() => this._sendKey("p")}"></paper-input>
+                      <paper-input class="btn-flat flat-high ripple" style="margin-top: 0px; height: 50%;border: 2px solid white;" placeholder="Search..." @keydown="${() => this._sendKey("p")}"></paper-input>
                   </div>
 
 <!-- ################################# MEDIA CONTROL ################################# -->
